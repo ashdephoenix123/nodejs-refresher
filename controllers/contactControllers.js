543395ee644ef20052e3dd3299b9e3361df6e3be
Akash <akash@appycodes.com>
2025-06-28 13:47:42 +0530

    first commit

---
+++ controllers/contactControllers.js
@@ -0,0 +1,56 @@
+const asyncHandler = require("express-async-handler");
+
+// @desc GET all contacts
+// @routes /api/contacts
+// @access public
+
+const getContacts = asyncHandler(async (req, res) => {
+  res.status(200).json({ data: "Get all Contacts!" });
+});
+
+// @desc POST contacts
+// @routes /api/contacts
+// @access public
+
+const createContact = asyncHandler(async (req, res) => {
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields i.e. name, email and phone are mandatory!");
+  }
+  res.status(201).json({
+    data: `Data created for ${name} \t email: ${email} \t phone: ${phone}`,
+  });
+});
+
+// @desc Get single contact
+// @routes /api/contacts
+// @access public
+
+const getContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Contact Get for id ${req.params.id}` });
+});
+
+// @desc UPDATE single contact
+// @routes /api/contacts
+// @access public
+
+const updateContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Contact Updated for id ${req.params.id}` });
+});
+
+// @desc UPDATE single contact
+// @routes /api/contacts
+// @access public
+
+const deleteContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Contact Deleted for id ${req.params.id}` });
+});
+
+module.exports = {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { errorHandler } = require("./errorHandler/errorHandler");
+const dotenv = require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/api/contact", require("./routes/contactRoutes"));
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server running on port http://localhost:${port}`);
+});
